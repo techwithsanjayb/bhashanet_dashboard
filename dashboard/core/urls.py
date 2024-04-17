b66e218367ca9fd7dacac36aa2f5726b593591ed
Sanjay Bhargava <sanjayb.info@gmail.com>
2024-04-17 15:11:59 +0530

    Working Code

--- dashboard/core/urls.py
+++ dashboard/core/urls.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+app_name = 'core'
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('chart', views.chart, name="chart"),
-    path('stackedchart', views.stackedchart, name="stackedchart"),
-    path('piechart', views.piechart, name="piechart"),
-    path('display_table', views.display_table, name="display_table"),
+    path('', views.home, name="home"),   
     path('display_table2', views.display_table2, name="display_table2"),
-    path('converturl', views.converturl, name="converturl"),
-   
+    path('update_json', views.update_json, name="update_json"),
+    path('urladd', views.urladd, name="urladd"),
+    
+     
 ]
